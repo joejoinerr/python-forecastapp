75d75baed69217baabdfed98c5f9df82ecf6d59b
Joe Joiner <2454320+joejoinerr@users.noreply.github.com>
2021-10-01 18:39:45 +0100

    Create setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(name='python-forecastapp',
+      version='0.1.0',
+      description='Python wrapper for the Forecast API',
+      author='Joe Joiner',
+      author_email='joe@legato.digital',
+      url='https://github.com/joejoinerr/python-forecastapp',
+      packages=['python-forecastapp'],
+      install_requires=[
+        'requests',
+      ])
