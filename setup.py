361fcdb10de6486b548eca1d2134c76872a3fbf3
Joe Joiner <2454320+joejoinerr@users.noreply.github.com>
2021-10-02 11:26:16 +0100

    Auto find packages in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(name='python-forecastapp',
-      version='0.1.1',
+      version='0.1.2',
       description='Python wrapper for the Forecast API',
       author='Joe Joiner',
       author_email='joe@legato.digital',
       url='https://github.com/joejoinerr/python-forecastapp',
-      packages=['forecast'],
+      packages=find_packages(),
       install_requires=[
         'requests',
       ])
