5cec88586ca05b39eba13489bf639045628d663c
Joe Joiner <2454320+joejoinerr@users.noreply.github.com>
2021-10-02 10:52:52 +0100

    Silly mistake

--- forecast/client.py
+++ forecast/client.py
@@ -22,7 +22,7 @@ class ForecastClient:
         self.projects = models.ProjectsHelper(self)
 
         # Interface for interacting with roles
-        self.projects = models.RolesHelper(self)
+        self.roles = models.RolesHelper(self)
 
         # Interface for interacting with NPT
         self.non_project_time = models.NPTHelper(self)
