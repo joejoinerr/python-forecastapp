869458fcbd58a6ffcf68b8a7f1ac9bed7ddeb710
Joe Joiner <2454320+joejoinerr@users.noreply.github.com>
2021-11-02 16:33:28 +0000

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='python-forecastapp',
-      version='0.1.2',
+      version='0.2.0',
       description='Python wrapper for the Forecast API',
       author='Joe Joiner',
       author_email='joe@legato.digital',
