2e9faa748a22a500d8d1f4e9f1e1be90a35fdea1
Joe Joiner <2454320+joejoinerr@users.noreply.github.com>
2021-11-03 17:51:56 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='python-forecastapp',
-      version='0.2.1',
+      version='0.2.2',
       description='Python wrapper for the Forecast API',
       author='Joe Joiner',
       author_email='joe@legato.digital',
